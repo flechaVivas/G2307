bada1a6ff6400cc4bb1147496b36744de1590d0a
Bautista Guerra <bauguerra@gmail.com>
2023-05-17 10:06:39 -0300

    practico_02 terminado
    practico_02 terminado

--- practico_02/ejercicio_01.py
+++ practico_02/ejercicio_01.py
@@ -18,6 +18,9 @@ Referencia: https://docs.python.org/3/reference/import.html#the-import-system
 """
 
 # Completar
+import main as main
+from source import util as util
+from source.controller import controller as controller
 
 
 # NO MODIFICAR - INICIO
