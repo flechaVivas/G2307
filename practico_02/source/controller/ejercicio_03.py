80a68f1a6ebd090f8fc9cdcf356ab259e795b39d
flechaVivas <facundovivas15@gmail.com>
2023-07-02 20:13:21 -0300

    Practico 2 terminado faltaban ejs 2 y 3

--- practico_02/source/controller/ejercicio_03.py
+++ practico_02/source/controller/ejercicio_03.py
@@ -12,6 +12,12 @@ $PATH$/practico_02> python -m source.controller.ejercicio_03
 """
 
 # Completar
+from . import controller
+import source.util as util
+import source.data.database as database
+import config.test_config as test_config
+import config.ci_cd.deploy_travis as deploy_travis
+import main as main
 
 # NO MODIFICAR - INICIO
 assert main.name == "main"
